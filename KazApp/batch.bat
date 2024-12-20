e6ea3a4c278a4694d2f169276369cc2d7f0cff4e
kazGear <165551135+kazGear@users.noreply.github.com>
2024-12-20 13:36:08 +0900

    バッチ修正

--- KazApp/batch.bat
+++ KazApp/batch.bat
@@ -5,6 +5,6 @@ cd c:\repository_kaz_app\kaz_app\KazApp\KazApi.NET
 dotnet publish -c Release -o build KazApi.sln
 
 REM モンスタ－オートバトル
-start /wait "C:\repository_kaz_app\kaz_app\KazApp\AutoBattle\bin\Release\net8.0\AutoBattle.exe"
+start "" "C:\repository_kaz_app\kaz_app\KazApp\AutoBattle\bin\Release\net8.0\AutoBattle.exe"
 
 pause
